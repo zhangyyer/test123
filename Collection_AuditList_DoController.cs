448c69a94261fb3d3847ee8349d1b04cd85336de
zhangyyer <863412197@qq.com>
2015-09-28 16:35:49 +0800

    retre
    etre4t54

---
+++ Collection_AuditList_DoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrmNewCustomer.Areas.Achievement.Controllers
+{
+    public class Collection_AuditList_DoController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
